4eeb7a8f09d99e77a6b67b16fa2dfc0dbdd0b8eb
Mihx222 <mihx222@gmail.com>
2019-12-02 14:10:24 +0200

    Adjusted gameplay

--- js/Level1.js
+++ js/Level1.js
@@ -4,9 +4,14 @@ var map;
 var layer;
 
 var player;
+var playerSpawnX = 100;
+var playerSpawnY = 800;
+var playerVelocityX = 250;
+var playerVelocityY = 600;
+
 var controls = {};
-var playerSpeed = 250;
 var jumpTimer = 0;
+var timerDelay = 750;
 
 Game.Level1.prototype = {
     create:function() {
@@ -21,14 +26,17 @@ Game.Level1.prototype = {
 
         map.setCollisionBetween(0, 60);
 
-        player = this.add.sprite(100, 460, 'big_ball');
+        // Make it so that the player is reset when the tile with indexes 5 and 19 is touched
+        // Note that in Tiled, the indexes start counting from 0, Phaser counts from 1.
+        map.setTileIndexCallback(5, this.resetPlayer, this);
+        map.setTileIndexCallback(19, this.resetPlayer, this);
+
+        player = this.add.sprite(playerSpawnX, playerSpawnY, 'big_ball');
         player.anchor.setTo(0.5, 0.5);
-        // player.animations.add('idle', [0, 1], 1, true);
         // player.animations.add('jump', [2], 1, true);
-        // player.animations.add('run', [3, 4, 5, 6, 7, 8], 7, true);
         this.physics.arcade.enable(player);
-        this.camera.follow(player, Phaser.Camera.FOLLOW_PLATFORMER);
         player.body.collideWorldBounds = true;
+        player.body.bounce.setTo(0.4);
 
         controls = {
             right: this.input.keyboard.addKey(Phaser.Keyboard.D),
@@ -38,22 +46,53 @@ Game.Level1.prototype = {
     },
 
     update:function() {
+        this.camera.follow(player, Phaser.Camera.FOLLOW_PLATFORMER);
         this.physics.arcade.collide(player, layer);
 
-        if (controls.right.isDown &&
-            (player.body.onFloor() || player.body.touching.down) &&
-            this.time.now > jumpTimer) {
-                player.body.velocity.y = -600;
-                player.body.velocity.x = playerSpeed;
-                jumpTimer = this.time.now + 750;
-        } else if (controls.left.isDown &&
-            (player.body.onFloor() || player.body.touching.down) &&
-            this.time.now > jumpTimer) {
-                player.body.velocity.y = -600;
-                player.body.velocity.x = -playerSpeed;
-                jumpTimer = this.time.now + 750;
-        } else if (!controls.left.isDown && !controls.right.isDown) {
+        // Player controls
+        // Pressing A or D executes a jump. Player only moves with jumps
+        // Player is also capable of moving midair
+        if (controls.right.isDown
+            && (player.body.onFloor() || player.body.touching.down)
+            && this.time.now > jumpTimer
+        ) {
+            player.body.velocity.y = -playerVelocityY;
+            player.body.velocity.x = playerVelocityX;
+            jumpTimer = this.time.now + timerDelay;
+        } else if (controls.right.isDown
+            && !(player.body.onFloor() || player.body.touching.down)
+        ) {
+            player.body.velocity.x = playerVelocityX;
+        }
+        
+        if (controls.left.isDown
+            && (player.body.onFloor() || player.body.touching.down)
+            && this.time.now > jumpTimer
+        ) {
+            player.body.velocity.y = -playerVelocityY;
+            player.body.velocity.x = -playerVelocityX;
+            jumpTimer = this.time.now + timerDelay;
+        } else if (controls.left.isDown
+            && !(player.body.onFloor() || player.body.touching.down)
+        ) {
+            player.body.velocity.x = -playerVelocityX;
+        }
+        
+        if (!controls.left.isDown && !controls.right.isDown) {
             player.body.velocity.x = 0;
         }
+    },
+
+    resetPlayer:function() {
+        this.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON);
+
+        // Make the ball smaller and faster
+        player.loadTexture('small_ball');
+        player.body.setSize(30, 27);
+        player.reset(playerSpawnX, playerSpawnY);
+
+        playerVelocityX = 350;
+        playerVelocityY = 450;
+        timerDelay = 500;
     }
 }
