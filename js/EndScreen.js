37b18d092e5c6adb934c2537e822eb97673c1f6a
mih97 <u.mihai97@gmail.com>
2019-12-05 14:40:09 +0200

    added power ups and end gate

---
+++ js/EndScreen.js
@@ -0,0 +1,14 @@
+Game.EndScreen = function(game){};
+
+Game.EndScreen.prototype = {
+    preload:function(game){
+
+    },
+
+    create:function(game){
+
+    },
+    update:function(game){
+        
+    }
+}
